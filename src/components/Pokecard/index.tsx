1e837323c52dde8cda88fedfad636f10e1e1df2e
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-31 18:38:16 +0330

    Change Pokecard to be a functional component.

--- src/components/Pokecard/index.tsx
+++ src/components/Pokecard/index.tsx
@@ -1,7 +1,5 @@
 import "./Pokecard.css";
 
-import { Component } from "react";
-
 export interface IPokecard {
   id: number;
   name: string;
@@ -9,30 +7,26 @@ export interface IPokecard {
   base_experience: number;
 }
 
-class Pokecard extends Component<IPokecard> {
-  render() {
-    const { id, name, type, base_experience } = this.props;
-
-    return (
-      <div className="Pokecard">
-        <div className="Pokecard__image_container">
-          <img
-            className="Pokecard__image"
-            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id
-              .toString()
-              .padStart(3, "0")}.png`}
-            alt={name}
-          />
-        </div>
-        <h2 className="Pokecard__name">{name}</h2>
-        <p className="Pokecard__type">
-          Type: <span>{type}</span>
-          <br />
-          Exp: <span>{base_experience}</span>
-        </p>
+const Pokecard: React.FC<IPokecard> = ({ id, name, type, base_experience }) => {
+  return (
+    <div className="Pokecard">
+      <div className="Pokecard__image_container">
+        <img
+          className="Pokecard__image"
+          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id
+            .toString()
+            .padStart(3, "0")}.png`}
+          alt={name}
+        />
       </div>
-    );
-  }
-}
+      <h2 className="Pokecard__name">{name}</h2>
+      <p className="Pokecard__type">
+        Type: <span>{type}</span>
+        <br />
+        Exp: <span>{base_experience}</span>
+      </p>
+    </div>
+  );
+};
 
 export default Pokecard;
