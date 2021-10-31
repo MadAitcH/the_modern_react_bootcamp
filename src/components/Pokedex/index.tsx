c1f7e63622c615334df438de423bcadf33995824
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-31 18:36:11 +0330

    Change Pokedex to be a functional component.

--- src/components/Pokedex/index.tsx
+++ src/components/Pokedex/index.tsx
@@ -1,6 +1,5 @@
 import "./Pokedex.css";
 
-import { Component } from "react";
 import Pokecard, { IPokecard } from "../Pokecard";
 
 interface PokedexProps {
@@ -9,32 +8,28 @@ interface PokedexProps {
   isWinner?: boolean;
 }
 
-class Pokedex extends Component<PokedexProps> {
-  render() {
-    return (
-      <div className="Pokedex">
-        <div className="Pokedex__status">
-          <h1
-            className={
-              this.props.isWinner
-                ? `Pokedex__status--winner`
-                : `Pokedex__status--loser`
-            }
-          >
-            {this.props.isWinner ? "Winning Hand" : "Losing Hand"}
-          </h1>
-          <h4>
-            Total Experience: <span>{this.props.totalExp}</span>
-          </h4>
-        </div>
-        <div className="Pokedex__cards">
-          {this.props.items.map((item) => (
-            <Pokecard {...item} key={item.id} />
-          ))}
-        </div>
+const Pokedex: React.FC<PokedexProps> = ({ isWinner, totalExp, items }) => {
+  return (
+    <div className="Pokedex">
+      <div className="Pokedex__status">
+        <h1
+          className={
+            isWinner ? `Pokedex__status--winner` : `Pokedex__status--loser`
+          }
+        >
+          {isWinner ? "Winning Hand" : "Losing Hand"}
+        </h1>
+        <h4>
+          Total Experience: <span>{totalExp}</span>
+        </h4>
       </div>
-    );
-  }
-}
+      <div className="Pokedex__cards">
+        {items.map((item) => (
+          <Pokecard {...item} key={item.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Pokedex;
