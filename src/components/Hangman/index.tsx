5d4bb126d4339b2dbaf4fc5c56434cf5ee8a04c6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-02 01:15:43 +0330

    Change the Hangman.js to Hangman.tsx and add apporopriate types.

---
+++ src/components/Hangman/index.tsx
@@ -0,0 +1,89 @@
+import "./Hangman.css";
+
+import { Component, MouseEvent } from "react";
+import img0 from "./0.jpg";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+
+interface HangmanProps {
+  maxWrong: number;
+  images: string[];
+}
+
+interface HangmanState {
+  nWrong: number;
+  guessed: Set<string>;
+  answer: string;
+}
+
+class Hangman extends Component<HangmanProps, HangmanState> {
+  /** by default, allow 6 guesses and use provided gallows images. */
+  static defaultProps: HangmanProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6],
+  };
+
+  constructor(props: HangmanProps) {
+    super(props);
+
+    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+
+    this.handleGuess = this.handleGuess.bind(this);
+  }
+
+  /** guessedWord: show current-state of word:
+    if guessed letters are {a,p,e}, show "app_e" for "apple"
+  */
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+  /** handleGuest: handle a guessed letter:
+    - add to guessed letters
+    - if not in answer, increase number-wrong guesses
+  */
+  handleGuess(e: MouseEvent<HTMLButtonElement>) {
+    let ltr = (e.target as HTMLButtonElement).value;
+
+    this.setState((st) => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+    }));
+  }
+
+  /** generateButtons: return array of letter buttons to render */
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr) => (
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  /** render: render game */
+  render() {
+    return (
+      <div className="Hangman">
+        <h1>Hangman</h1>
+        <img
+          src={this.props.images[this.state.nWrong]}
+          alt={`${this.state.nWrong}/${this.props.maxWrong} wronges guesses`}
+        />
+        <p className="Hangman-word">{this.guessedWord()}</p>
+        <p className="Hangman-btns">{this.generateButtons()}</p>
+      </div>
+    );
+  }
+}
+
+export default Hangman;
