78625cd2a1ae8b680aa111fe5a6de1147b02d7c5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-01 20:31:58 +0330

    Change Coin to be a functional component.

--- src/components/Coin/index.tsx
+++ src/components/Coin/index.tsx
@@ -1,6 +1,6 @@
 import "./Coin.css";
 
-import { Component } from "react";
+import { FC } from "react";
 import heads from "./heads.jpg";
 import tails from "./tails.jpg";
 
@@ -8,18 +8,16 @@ interface CoinProps {
   face: "tails" | "heads";
 }
 
-class Coin extends Component<CoinProps> {
-  render() {
-    return (
-      <div className="Coin">
-        <img
-          className="Coin__image"
-          src={this.props.face === "heads" ? heads : tails}
-          alt={this.props.face}
-        />
-      </div>
-    );
-  }
-}
+const Coin: FC<CoinProps> = ({ face }) => {
+  return (
+    <div className="Coin">
+      <img
+        className="Coin__image"
+        src={face === "heads" ? heads : tails}
+        alt={face}
+      />
+    </div>
+  );
+};
 
 export default Coin;
