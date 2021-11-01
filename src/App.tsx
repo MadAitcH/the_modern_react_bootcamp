2af5c8c8db0c7e3af3f4451ec5c6369668dd7523
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-01 22:13:21 +0330

    Change ColorPalette to a functional component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,27 @@ import ColorPalette from "./components/ColorPalette";
 function App() {
   return (
     <div className="App">
-      <ColorPalette />
+      <ColorPalette
+        boxCount={18}
+        palette={[
+          "pink",
+          "yellow",
+          "#137174",
+          "rgb(99,199,29)",
+          "rgba(255, 93, 38, 200)",
+          "gold",
+          "whitesmoke",
+          "orange",
+          "bisque",
+          "brown",
+          "burlywood",
+          "cornflowerblue",
+          "crimson",
+          "darkorange",
+          "darkseagreen",
+          "tomato",
+        ]}
+      />
     </div>
   );
 }
--- src/components/ColorPalette/index.tsx
+++ src/components/ColorPalette/index.tsx
@@ -1,6 +1,6 @@
 import "./ColroPalette.css";
 
-import { Component } from "react";
+import { FC } from "react";
 import ColorBox from "../ColorBox";
 
 interface ColorPaletteProps {
@@ -8,38 +8,14 @@ interface ColorPaletteProps {
   boxCount: number;
 }
 
-class ColorPalette extends Component<ColorPaletteProps> {
-  static defaultProps = {
-    palette: [
-      "pink",
-      "yellow",
-      "#137174",
-      "rgb(99,199,29)",
-      "rgba(255, 93, 38, 200)",
-      "gold",
-      "whitesmoke",
-      "orange",
-      "bisque",
-      "brown",
-      "burlywood",
-      "cornflowerblue",
-      "crimson",
-      "darkorange",
-      "darkseagreen",
-      "tomato",
-    ],
-    boxCount: 18,
-  };
-
-  render() {
-    return (
-      <div className="ColorPalette">
-        {Array.from({ length: this.props.boxCount }).map(() => (
-          <ColorBox palette={this.props.palette} />
-        ))}
-      </div>
-    );
-  }
-}
+const ColorPalette: FC<ColorPaletteProps> = ({ boxCount, palette }) => {
+  return (
+    <div className="ColorPalette">
+      {Array.from({ length: boxCount }).map(() => (
+        <ColorBox palette={palette} />
+      ))}
+    </div>
+  );
+};
 
 export default ColorPalette;
