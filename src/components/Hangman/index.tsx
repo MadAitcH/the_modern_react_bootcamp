ee12af7af16367c1e4107b0959fdcc425944b4a7
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-02 03:44:17 +0330

    Change Hangman to a functional component.

--- src/components/Hangman/index.tsx
+++ src/components/Hangman/index.tsx
@@ -1,6 +1,6 @@
 import "./Hangman.css";
 
-import { Component, MouseEvent } from "react";
+import { FC, MouseEvent, useState } from "react";
 import img0 from "./0.jpg";
 import img1 from "./1.jpg";
 import img2 from "./2.jpg";
@@ -13,101 +13,68 @@ import { checkGuess } from "../../utils";
 import AlphaButtons from "../AlphaButtons";
 
 interface HangmanProps {
-  maxWrong: number;
-  images: string[];
+  maxWrong?: number;
+  images?: string[];
 }
 
-interface HangmanState {
-  nWrong: number;
-  guessed: Set<string>;
-  answer: string;
-}
+const defaultProps = {
+  maxWrong: 6,
+  images: [img0, img1, img2, img3, img4, img5, img6],
+};
 
-class Hangman extends Component<HangmanProps, HangmanState> {
-  /** by default, allow 6 guesses and use provided gallows images. */
-  static defaultProps: HangmanProps = {
-    maxWrong: 6,
-    images: [img0, img1, img2, img3, img4, img5, img6],
-  };
+const Hangman: FC<HangmanProps> = ({
+  maxWrong = defaultProps.maxWrong,
+  images = defaultProps.images,
+}) => {
+  const [nWrong, setNWrong] = useState(0);
+  const [answer, setAnswer] = useState(randomWord());
+  const [guessed, setGuessed] = useState<Set<string>>(new Set());
 
-  constructor(props: HangmanProps) {
-    super(props);
-
-    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
-
-    this.handleGuess = this.handleGuess.bind(this);
-    this.onRestartClick = this.onRestartClick.bind(this);
-  }
-
-  /** guessedWord: show current-state of word:
-    if guessed letters are {a,p,e}, show "app_e" for "apple"
-  */
-  guessedWord() {
-    return this.state.answer
-      .split("")
-      .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"));
-  }
-
-  /** handleGuest: handle a guessed letter:
-    - add to guessed letters
-    - if not in answer, increase number-wrong guesses
-  */
-  handleGuess(e: MouseEvent<HTMLButtonElement>) {
+  const handleGuess = (e: MouseEvent<HTMLButtonElement>) => {
     let ltr = (e.target as HTMLButtonElement).value;
 
-    this.setState((st) => ({
-      guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
-    }));
-  }
+    setGuessed(new Set(guessed.add(ltr)));
+    setNWrong(nWrong + (answer.includes(ltr) ? 0 : 1));
+  };
 
-  onRestartClick() {
-    this.setState({
-      answer: randomWord(),
-      nWrong: 0,
-      guessed: new Set(),
-    });
-  }
+  const onRestartClick = () => {
+    setAnswer(randomWord());
+    setNWrong(0);
+    setGuessed(new Set());
+  };
 
-  /** render: render game */
-  render() {
-    return (
-      <div className="Hangman">
-        <h1>Hangman</h1>
-        <img
-          src={this.props.images[this.state.nWrong]}
-          alt={`${this.state.nWrong}/${this.props.maxWrong} wrong guesses`}
-        />
-        <p className="Hangman-wrong">Wrong Guesses: {this.state.nWrong}</p>
-        <p className="Hangman-word">
-          {this.state.nWrong < this.props.maxWrong
-            ? this.guessedWord()
-            : this.state.answer}
-        </p>
+  const guessedWord = () => {
+    return answer.split("").map((ltr) => (guessed.has(ltr) ? ltr : "_"));
+  };
 
-        {checkGuess(this.state.guessed, this.state.answer) ? (
-          <div>
-            <h2>You Won!</h2>
-            <button onClick={this.onRestartClick} className="Hangman-restart">
-              Restart!
-            </button>
-          </div>
-        ) : this.state.nWrong < this.props.maxWrong ? (
-          <AlphaButtons
-            guessed={this.state.guessed}
-            handleGuess={this.handleGuess}
-          />
-        ) : (
-          <div>
-            <h2>Game Over</h2>
-            <button onClick={this.onRestartClick} className="Hangman-restart">
-              Restart!
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Hangman">
+      <h1>Hangman</h1>
+      <img src={images[nWrong]} alt={`${nWrong}/${maxWrong} wrong guesses`} />
+      <p className="Hangman-wrong">Wrong Guesses: {nWrong}</p>
+      <p className="Hangman-word">
+        {nWrong < maxWrong ? guessedWord() : answer}
+      </p>
+
+      {checkGuess(guessed, answer) ? (
+        <div>
+          <h2>You Won!</h2>
+          <button onClick={onRestartClick} className="Hangman-restart">
+            Restart!
+          </button>
+        </div>
+      ) : nWrong < maxWrong ? (
+        <AlphaButtons guessed={guessed} handleGuess={handleGuess} />
+      ) : (
+        <div>
+          <h2>Game Over</h2>
+          <button onClick={onRestartClick} className="Hangman-restart">
+            Restart!
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Hangman;
