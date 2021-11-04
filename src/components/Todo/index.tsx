73bc9a452201fb2411f0dc0c0e479e45c3a1aa04
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-04 22:21:19 +0330

    Change the Todo component to a functional one.

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,6 +1,6 @@
 import "./Todo.css";
 
-import { ChangeEvent, Component } from "react";
+import { ChangeEvent, FC, useState } from "react";
 
 export interface ITodo {
   task: string;
@@ -15,98 +15,83 @@ type TodoProps = ITodo & {
   completeTodo: (id: string) => void;
 };
 
-interface TodoState {
-  task: string;
-  isEditing: boolean;
-}
+const Todo: FC<TodoProps> = ({
+  task,
+  id,
+  completed,
+  removeTodo,
+  editTodo,
+  completeTodo,
+}) => {
+  const [text, setText] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
 
-class Todo extends Component<TodoProps, TodoState> {
-  constructor(props: TodoProps) {
-    super(props);
+  const onRemoveClick = () => {
+    removeTodo(id);
+  };
 
-    this.state = {
-      task: this.props.task,
-      isEditing: false,
-    };
+  const onEditClick = () => {
+    setIsEditing(true);
+  };
 
-    this.onRemoveClick = this.onRemoveClick.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onTodoClick = this.onTodoClick.bind(this);
-    this.onEditClick = this.onEditClick.bind(this);
-    this.onSaveClick = this.onSaveClick.bind(this);
-  }
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
 
-  onRemoveClick() {
-    this.props.removeTodo(this.props.id);
-  }
+  const onSaveClick = () => {
+    setIsEditing(false);
+    editTodo(id, text);
+  };
 
-  onEditClick() {
-    this.setState({ isEditing: true });
-  }
+  const onTodoClick = () => {
+    completeTodo(id);
+  };
 
-  onInputChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      task: e.target.value,
-    });
-  }
+  const result = isEditing ? (
+    <div className="Todo-edit-form">
+      <input
+        type="text"
+        value={text}
+        name="text"
+        onChange={onInputChange}
+        className="Todo__edit--input"
+      />
+      <button className="Todo__edit--save" onClick={onSaveClick}>
+        Save
+      </button>
+    </div>
+  ) : (
+    <div className="task-text">
+      <li className={`Todo-task`} onClick={onTodoClick}>
+        {task}
+      </li>
+    </div>
+  );
 
-  onSaveClick() {
-    this.setState({ isEditing: false });
+  return (
+    <div className={`Todo ${completed && "completed"}`}>
+      {result}
 
-    this.props.editTodo(this.props.id, this.state.task);
-  }
-
-  onTodoClick() {
-    this.props.completeTodo(this.props.id);
-  }
-
-  render() {
-    let result = this.state.isEditing ? (
-      <div className="Todo-edit-form">
-        <input
-          type="text"
-          value={this.state.task}
-          name="task"
-          onChange={this.onInputChange}
-          className="Todo__edit--input"
-        />
-        <button className="Todo__edit--save" onClick={this.onSaveClick}>
-          Save
-        </button>
-      </div>
-    ) : (
-      <div className="task-text">
-        <li className={`Todo-task`} onClick={this.onTodoClick}>
-          {this.props.task}
-        </li>
-      </div>
-    );
-
-    return (
-      <div className={`Todo ${this.props.completed && "completed"}`}>
-        {result}
-
-        {!this.state.isEditing && (
-          <div className="Todo-buttons">
-            <button
-              title="Edit task"
-              className="Todo__edit--button"
-              onClick={this.onEditClick}
-            >
-              &#9998;
-            </button>
-            <button
-              title="Remove task"
-              className="Todo__remove"
-              onClick={this.onRemoveClick}
-            >
-              &#10007;
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+      {!isEditing && (
+        <div className="Todo-buttons">
+          <button
+            title="Edit task"
+            className="Todo__edit--button"
+            onClick={onEditClick}
+          >
+            &#9998;
+          </button>
+          <button
+            title="Remove task"
+            className="Todo__remove"
+            onClick={onRemoveClick}
+          >
+            &#10007;
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Todo;
