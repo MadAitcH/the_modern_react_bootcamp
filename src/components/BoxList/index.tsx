e75ea116aa85510f4b5f24e833e22a9abdc3cc06
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-04 06:49:57 +0330

    Change BoxList to be a functional component.

--- src/components/BoxList/index.tsx
+++ src/components/BoxList/index.tsx
@@ -1,63 +1,39 @@
 import "./BoxList.css";
 
-import { Component } from "react";
+import { FC, useState } from "react";
 import Box from "../Box";
 import NewBoxForm, { Box as IBox } from "../NewBoxForm";
 
-interface BoxListState {
-  boxList: IBox[];
-}
+const BoxList: FC = () => {
+  const [boxList, setBoxList] = useState<IBox[]>([]);
 
-// TODO: fix any types
-class BoxList extends Component<any, BoxListState> {
-  constructor(props: any) {
-    super(props);
+  const addBox = (newBox: IBox) => {
+    setBoxList([...boxList, newBox]);
+  };
 
-    this.state = {
-      boxList: [],
-    };
+  const removeBox = (id: string) => {
+    setBoxList(boxList.filter((box) => box.id !== id));
+  };
 
-    this.addBox = this.addBox.bind(this);
-    this.removeBox = this.removeBox.bind(this);
-  }
-
-  addBox(newBox: IBox) {
-    this.setState((st) => {
-      return {
-        boxList: [...st.boxList, newBox],
-      };
-    });
-  }
-
-  removeBox(id: string) {
-    this.setState((st) => {
-      return {
-        boxList: st.boxList.filter((box) => box.id !== id),
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div className="BoxList">
-        <NewBoxForm addBox={this.addBox} />
-        <div className="BoxList__boxes">
-          {this.state.boxList.map(({ width, height, id, backgroundColor }) => {
-            return (
-              <Box
-                key={id}
-                width={width}
-                height={height}
-                id={id}
-                bgColor={backgroundColor}
-                removeBox={this.removeBox}
-              />
-            );
-          })}
-        </div>
+  return (
+    <div className="BoxList">
+      <NewBoxForm addBox={addBox} />
+      <div className="BoxList__boxes">
+        {boxList.map(({ width, height, id, backgroundColor }) => {
+          return (
+            <Box
+              key={id}
+              width={width}
+              height={height}
+              id={id}
+              bgColor={backgroundColor}
+              removeBox={removeBox}
+            />
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BoxList;
