d3e770c7c59ab92148620566f9842331555e1893
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-05 19:35:02 +0330

    Add Die compnent and add some styling to it.
    * Install @fortawesome/fontawesome-free.
    * TODO: finish handleClick function.

---
+++ src/components/Die/index.tsx
@@ -0,0 +1,66 @@
+import "./Die.css";
+
+import { Component } from "react";
+
+interface DieProps {
+  locked: boolean;
+  idx: number;
+  val: number;
+  handleClick: () => void;
+}
+
+interface DieState {
+  isRolling: boolean;
+}
+
+const faces: { [key: number]: string } = {
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+};
+
+class Die extends Component<DieProps, DieState> {
+  constructor(props: DieProps) {
+    super(props);
+
+    this.state = {
+      isRolling: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.isRolling) return;
+
+    this.setState({
+      isRolling: true,
+    });
+
+    // TODO: handle click
+
+    setTimeout(() => {
+      this.setState({ isRolling: false });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <button
+        className={`Die ${
+          this.state.isRolling && !this.props.locked && "Die-rolling"
+        } ${this.props.locked && "Die-locked"}`}
+        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+        onClick={this.props.handleClick}
+        disabled={this.props.locked}
+      >
+        <i className={`fas fa-dice-${faces[this.props.val]}`} />
+      </button>
+    );
+  }
+}
+
+export default Die;
