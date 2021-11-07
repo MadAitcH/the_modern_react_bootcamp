d237e2e01f3cd7aafcc1c8219785ac838a9d9a7a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-08 01:07:33 +0330

    Change Game component to a functional component.

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,171 +1,149 @@
 import "./Game.css";
 
-import { Component } from "react";
+import { FC, useEffect, useState } from "react";
 import Dice from "../Dice";
 import ScoreTable, { Score } from "../ScoreTable";
 
-interface GameState {
-  dice: number[];
-  locked: boolean[];
-  rollsLeft: number;
-  rolling: boolean;
-  isOver: boolean;
-  scores: {
-    [key in Score]: number | undefined;
-  };
-}
-
 const NUM_DICE = 5;
 const NUM_ROLLS = 3;
-const INITIAL_STATE: GameState = {
-  dice: Array.from({ length: NUM_DICE }),
-  locked: Array(NUM_DICE).fill(false),
-  rolling: false,
-  rollsLeft: NUM_ROLLS,
-  isOver: false,
-  scores: {
-    ones: undefined,
-    twos: undefined,
-    threes: undefined,
-    fours: undefined,
-    fives: undefined,
-    sixes: undefined,
-    threeOfKind: undefined,
-    fourOfKind: undefined,
-    fullHouse: undefined,
-    smallStraight: undefined,
-    largeStraight: undefined,
-    yahtzee: undefined,
-    chance: undefined,
-  },
+
+const initialScores = {
+  ones: undefined,
+  twos: undefined,
+  threes: undefined,
+  fours: undefined,
+  fives: undefined,
+  sixes: undefined,
+  threeOfKind: undefined,
+  fourOfKind: undefined,
+  fullHouse: undefined,
+  smallStraight: undefined,
+  largeStraight: undefined,
+  yahtzee: undefined,
+  chance: undefined,
 };
 
-class Game extends Component<any, GameState> {
-  constructor(props: any) {
-    super(props);
+const initialDice: number[] = Array.from({ length: NUM_DICE });
+const initialLocked: boolean[] = Array(NUM_DICE).fill(false);
 
-    this.state = INITIAL_STATE;
+const Game: FC = () => {
+  const [dice, setDice] = useState<number[]>(initialDice);
+  const [locked, setLocked] = useState<boolean[]>(initialLocked);
+  const [rolling, setRolling] = useState(false);
+  const [rollsLeft, setRollsLeft] = useState(NUM_ROLLS);
+  const [isOver, setIsOver] = useState(false);
+  const [scores, setScores] = useState<{
+    [key in Score]: number | undefined;
+  }>(initialScores);
 
-    this.roll = this.roll.bind(this);
-    this.doScore = this.doScore.bind(this);
-    this.toggleLocked = this.toggleLocked.bind(this);
-    this.animateRoll = this.animateRoll.bind(this);
-    this.displayRollInfo = this.displayRollInfo.bind(this);
-    this.onRestartClick = this.onRestartClick.bind(this);
-  }
+  useEffect(() => {
+    animateRoll();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  componentDidMount() {
-    this.animateRoll();
-  }
+  useEffect(() => {
+    if (isOver) return;
 
-  componentDidUpdate() {
-    if (this.state.isOver) return;
-
-    for (let k in this.state.scores) {
-      if (this.state.scores[k as Score] === undefined) return;
+    for (let k in scores) {
+      if (scores[k as Score] === undefined) return;
     }
 
-    this.setState({ isOver: true, rollsLeft: 0 });
-  }
+    setIsOver(true);
+    setRollsLeft(0);
+  }, [isOver, scores]);
 
-  onRestartClick() {
-    this.setState(INITIAL_STATE, () => this.animateRoll());
-  }
+  const animateRoll = () => {
+    setRolling(true);
 
-  roll() {
+    setTimeout(roll, 1000);
+  };
+
+  const roll = () => {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
-      dice: st.dice.map((d, i) =>
-        st.locked[i] ? d : Math.ceil(Math.random() * 6)
-      ),
-      locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft >= 1 ? st.rollsLeft - 1 : 0,
-      rolling: false,
-    }));
-  }
+    setDice(dice.map((d, i) => (locked[i] ? d : Math.ceil(Math.random() * 6))));
+    setLocked(rollsLeft > 1 ? locked : Array(NUM_DICE).fill(true));
+    setRollsLeft(rollsLeft >= 1 ? rollsLeft - 1 : 0);
+    setRolling(false);
+  };
 
-  animateRoll() {
-    this.setState({ rolling: true }, () => {
-      setTimeout(this.roll, 1000);
-    });
-  }
+  const onRestartClick = () => {
+    setDice(initialDice);
+    setLocked(initialLocked);
+    setRolling(false);
+    setRollsLeft(NUM_ROLLS);
+    setIsOver(false);
+    setScores(initialScores);
 
-  toggleLocked(idx: number) {
+    animateRoll();
+  };
+
+  const toggleLocked = (idx: number) => {
     // toggle whether idx is in locked or not
-    if (this.state.rollsLeft > 0 && !this.state.rolling) {
-      this.setState(st => ({
-        locked: [
-          ...st.locked.slice(0, idx),
-          !st.locked[idx],
-          ...st.locked.slice(idx + 1),
-        ],
-      }));
+    if (rollsLeft > 0 && !rolling) {
+      setLocked([
+        ...locked.slice(0, idx),
+        !locked[idx],
+        ...locked.slice(idx + 1),
+      ]);
     }
-  }
+  };
 
-  doScore(rulename: string, ruleFn: (dice: number[]) => number) {
+  const doScore = (rulename: string, ruleFn: (dice: number[]) => number) => {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
-      scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
-      rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false),
-    }));
-    this.animateRoll();
-  }
+    setScores({ ...scores, [rulename]: ruleFn(dice) });
+    setRollsLeft(NUM_ROLLS);
+    setLocked(Array(NUM_DICE).fill(false));
 
-  displayRollInfo() {
+    animateRoll();
+  };
+
+  const displayRollInfo = () => {
     const messages = [
       "0 Rolls Left",
       "1 Roll Left",
       "2 Rolls Left",
       "Starting Round",
     ];
-    return messages[this.state.rollsLeft || 0];
-  }
+    return messages[rollsLeft || 0];
+  };
 
-  render() {
-    return (
-      <div className="Game">
-        <header className="Game-header">
-          <h1 className="App-title">Yahtzee!</h1>
+  return (
+    <div className="Game">
+      <header className="Game-header">
+        <h1 className="App-title">Yahtzee!</h1>
 
-          <section className="Game-dice-section">
-            <Dice
-              disabled={this.state.rollsLeft <= 0}
-              rolling={this.state.rolling}
-              dice={this.state.dice}
-              locked={this.state.locked}
-              handleClick={this.toggleLocked}
-            />
-            <div className="Game-button-wrapper">
-              <button
-                className="Game-reroll"
-                disabled={
-                  this.state.locked.every(x => x) ||
-                  this.state.rolling ||
-                  this.state.rollsLeft <= 0
-                }
-                onClick={this.animateRoll}
-              >
-                {this.displayRollInfo()}
-              </button>
-            </div>
-          </section>
-        </header>
-
-        {this.state.isOver ? (
-          <div className="Game-over">
-            <h1>Game Over</h1>
-            <button className="Game-restart" onClick={this.onRestartClick}>
-              Restart
+        <section className="Game-dice-section">
+          <Dice
+            disabled={rollsLeft <= 0}
+            rolling={rolling}
+            dice={dice}
+            locked={locked}
+            handleClick={toggleLocked}
+          />
+          <div className="Game-button-wrapper">
+            <button
+              className="Game-reroll"
+              disabled={locked.every(x => x) || rolling || rollsLeft <= 0}
+              onClick={animateRoll}
+            >
+              {displayRollInfo()}
             </button>
           </div>
-        ) : (
-          <ScoreTable doScore={this.doScore} scores={this.state.scores} />
-        )}
-      </div>
-    );
-  }
-}
+        </section>
+      </header>
+
+      {isOver ? (
+        <div className="Game-over">
+          <h1>Game Over</h1>
+          <button className="Game-restart" onClick={onRestartClick}>
+            Restart
+          </button>
+        </div>
+      ) : (
+        <ScoreTable doScore={doScore} scores={scores} />
+      )}
+    </div>
+  );
+};
 
 export default Game;
