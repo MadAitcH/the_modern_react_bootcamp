31f0b7b5bd64bf46b8852f53ec1bf7b075a488ce
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-09 02:58:14 +0330

    Change Joke to a functional component.

--- src/components/Joke/index.tsx
+++ src/components/Joke/index.tsx
@@ -1,5 +1,5 @@
 import "./Joke.css";
-import { Component, MouseEvent } from "react";
+import { FC, MouseEvent } from "react";
 
 interface JokeProps {
   id: string;
@@ -8,86 +8,76 @@ interface JokeProps {
   text: string;
 }
 
-class Joke extends Component<JokeProps> {
-  constructor(props: JokeProps) {
-    super(props);
-
-    this.getColor = this.getColor.bind(this);
-    this.getEmoji = this.getEmoji.bind(this);
-    this.onVoteClick = this.onVoteClick.bind(this);
-  }
-
-  getColor() {
-    if (this.props.votes >= 15) {
+const Joke: FC<JokeProps> = ({ id, onVote, votes, text }) => {
+  const getColor = () => {
+    if (votes >= 15) {
       return "#4CAF50";
-    } else if (this.props.votes >= 12) {
+    } else if (votes >= 12) {
       return "#8BC34A";
-    } else if (this.props.votes >= 9) {
+    } else if (votes >= 9) {
       return "#CDDC39";
-    } else if (this.props.votes >= 6) {
+    } else if (votes >= 6) {
       return "#FFEB3B";
-    } else if (this.props.votes >= 3) {
+    } else if (votes >= 3) {
       return "#FFC107";
-    } else if (this.props.votes >= 0) {
+    } else if (votes >= 0) {
       return "#FF9800";
     } else {
       return "#f44336";
     }
-  }
+  };
 
-  getEmoji() {
-    if (this.props.votes >= 15) {
+  const getEmoji = () => {
+    if (votes >= 15) {
       return "em em-rolling_on_the_floor_laughing";
-    } else if (this.props.votes >= 12) {
+    } else if (votes >= 12) {
       return "em em-laughing";
-    } else if (this.props.votes >= 9) {
+    } else if (votes >= 9) {
       return "em em-smiley";
-    } else if (this.props.votes >= 6) {
+    } else if (votes >= 6) {
       return "em em-slightly_smiling_face";
-    } else if (this.props.votes >= 3) {
+    } else if (votes >= 3) {
       return "em em-neutral_face";
-    } else if (this.props.votes >= 0) {
+    } else if (votes >= 0) {
       return "em em-confused";
     } else {
       return "em em-angry";
     }
-  }
+  };
 
-  onVoteClick(e: MouseEvent<HTMLElement>) {
+  const onVoteClick = (e: MouseEvent<HTMLElement>) => {
     const buttonType = (e.target as HTMLElement).dataset["type"];
 
     if (buttonType === "upVote" || buttonType === "downVote") {
-      this.props.onVote(this.props.id, buttonType);
+      onVote(id, buttonType);
     } else {
       return;
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="Joke">
-        <div className="Joke-buttons">
-          <i
-            className="fas fa-arrow-up"
-            data-type="upVote"
-            onClick={this.onVoteClick}
-          />
-          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
-            {this.props.votes}
-          </span>
-          <i
-            className="fas fa-arrow-down"
-            data-type="downVote"
-            onClick={this.onVoteClick}
-          />
-        </div>
-        <div className="Joke-text">{this.props.text}</div>
-        <div className="Joke-smiley">
-          <i className={this.getEmoji()} />
-        </div>
+  return (
+    <div className="Joke">
+      <div className="Joke-buttons">
+        <i
+          className="fas fa-arrow-up"
+          data-type="upVote"
+          onClick={onVoteClick}
+        />
+        <span className="Joke-votes" style={{ borderColor: getColor() }}>
+          {votes}
+        </span>
+        <i
+          className="fas fa-arrow-down"
+          data-type="downVote"
+          onClick={onVoteClick}
+        />
       </div>
-    );
-  }
-}
+      <div className="Joke-text">{text}</div>
+      <div className="Joke-smiley">
+        <i className={getEmoji()} />
+      </div>
+    </div>
+  );
+};
 
 export default Joke;
