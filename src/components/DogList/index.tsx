16da202ede37b77e42f52897a50a74fceeaf1d52
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-11 03:10:08 +0330

    Create DogDetails and DogList components.

---
+++ src/components/DogList/index.tsx
@@ -0,0 +1,30 @@
+import "./DogList.css";
+
+import { Component } from "react";
+import { Dog } from "../../App";
+
+interface DogListProps {
+  dogs: Dog[];
+}
+
+class DogList extends Component<DogListProps> {
+  render() {
+    return (
+      <div className="DogList">
+        <h1 className="dispaly-1 text-center">Dog List!</h1>
+        <div className="container">
+          <div className="row">
+            {this.props.dogs.map(d => (
+              <div className="Dog col-md-6 col-lg-4 text-center">
+                <img src={d.src} alt={d.name} />
+                <h3>{d.name}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DogList;
