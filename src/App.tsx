776f660b2fde99c9c830e5348506c6bc29e07f4f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-10 21:37:46 +0330

    The whole project without any styling, with react-router-dom v 5.3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import VendingMachine from "./components/VendingMachine";
+import Soda from "./components/Soda";
+import Chips from "./components/Chips";
+import Sardines from "./components/Sardines";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact activeClassName="link-active" to="/">
+                Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink exact activeClassName="link-active" to="/soda">
+                Soda
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact activeClassName="link-active" to="/chips">
+                Chips
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact activeClassName="link-active" to="/sardines">
+                Sardines
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact render={() => <VendingMachine />} />
+          <Route path="/soda" exact render={() => <Soda />} />
+          <Route path="/chips" exact render={() => <Chips />} />
+          <Route path="/sardines" exact render={() => <Sardines />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/VendingMachine/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class VendingMachine extends Component {
+  render() {
+    return <div className="VendingMachine">Home</div>;
+  }
+}
+
+export default VendingMachine;
