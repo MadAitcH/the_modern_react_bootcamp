46eb95b4d7f364a14be60e8780bb9ac603152ba4
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-13 10:32:28 +0330

    Complete Copy Overlay

--- src/components/ColorBox/index.tsx
+++ src/components/ColorBox/index.tsx
@@ -25,6 +25,10 @@ const ColorBox: FC<ColorBoxProps> = ({ name, background }) => {
           className={`copy-overlay ${copied ? "show" : ""}`}
           style={{ background }}
         />
+        <div className={`copy-msg ${copied ? "show" : ""}`}>
+          <h1>copied!</h1>
+          <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
