4a8363a56947279d9a8ef7a2dee55e6ae5876219
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-15 12:23:59 +0330

    Start styling PaletteList withStyles

--- src/components/PaletteList/index.tsx
+++ src/components/PaletteList/index.tsx
@@ -1,20 +1,60 @@
 import { FC } from "react";
+import { withStyles, WithStyles } from "@mui/styles";
 import { IPalette } from "../../utils/seedColors";
 import MiniPalette from "../MiniPalette";
 
-interface PaletteListProps {
+const styles: { [key: string]: any } = {
+  root: {
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+
+  nav: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    color: "white",
+  },
+
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
+
+interface PaletteListProps extends WithStyles<typeof styles> {
   palettes: IPalette[];
 }
 
-const PaletteList: FC<PaletteListProps> = ({ palettes }) => {
+const PaletteList: FC<PaletteListProps> = ({ palettes, classes }) => {
   return (
-    <div className="PaletteList">
-      <h1>React Colors</h1>
-      {palettes.map(palette => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map(palette => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
