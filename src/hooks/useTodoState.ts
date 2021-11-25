8ea118be094881a90d795e60f3bf7d5c7fd9b749
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-11-25 12:28:06 +0330

    Create useTodoState hook, but don't ues it.

---
+++ src/hooks/useTodoState.ts
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ITodo } from "../components/TodoItem";
+
+export default function useTodoState(initialValue: ITodo[] = []) {
+  const [todos, setTodos] = useState<ITodo[]>(initialValue);
+
+  const addTodo = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        task: text,
+        completed: false,
+      },
+    ]);
+  };
+
+  const removeTodo = (todoId: string) => {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  };
+
+  const toggleCompletion = (todoId: string) => {
+    setTodos(
+      todos.map(todo =>
+        todoId === todo.id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (todoId: string, task: string) => {
+    setTodos(
+      todos.map(todo => (todoId === todo.id ? { ...todo, task } : todo))
+    );
+  };
+
+  return {
+    todos,
+    addTodo,
+    removeTodo,
+    toggleCompletion,
+    editTodo,
+  };
+}
